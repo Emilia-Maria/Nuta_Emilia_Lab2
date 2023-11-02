62565085072a492c1551b216eec95e6416057b0a
Emilia-Maria <nuta.emilia01@gmail.com>
2023-11-02 18:01:58 +0200

    Laborator intreg functional fara sarcina

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Nuta_Emilia_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,12 @@
+﻿using Nuta_Emilia_Lab2.Models;
+
+namespace Nuta_Emilia_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
